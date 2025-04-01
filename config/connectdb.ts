784aeae845bd75f8f8b30738a96d1d5bd56d14f2
unknown <tienduy12241224@gmail.com>
2025-04-01 11:33:23 +0700

    xoa file ts

--- config/connectdb.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const db = async () =>{
-    await mongoose.connect("mongodb://localhost:27017/DuyStella")
-    console.log("kết nối thành công");
-}
-export default db;
